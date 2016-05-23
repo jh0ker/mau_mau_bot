4a51c7bfb6016f26f57725140d7d12c6ef2f1e1e
Jannes Höke <jhoeke@uni-bremen.de>
2016-05-23 13:30:57 +0200

    change loglevel to INFO

--- bot.py
+++ bot.py
@@ -47,7 +47,7 @@ from simple_commands import help
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.DEBUG)
+    level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
