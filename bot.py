9dde74381eca860a6d65f4b42e6b49e105443dd8
Karho <Karho@users.noreply.github.com>
2017-03-04 09:17:52 +0800

    Update bot.py
    Removing Typos

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # Telegram bot to play UNO in group chats
-# Copyright (c) 2016-2017 Jannes Höke <uno@jhoeke.de> and Karho Yau
+# Copyright (c) 2016 - 2017 Jannes Höke <uno@jhoeke.de> and Karho Yau
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
