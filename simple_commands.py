89b01069c29514f73c9ca527fa772249d482ca8a
Pesakitan22 <pesakitan22@about.me>
2018-04-17 11:22:59 +0700

    templating

--- simple_commands.py
+++ simple_commands.py
@@ -72,7 +72,7 @@ attributions = ("Attributions:\n"
                 "Originals available on http://game-icons.net\n"
                 "Icons edited by ɳick")
 
-modes_explanation = ("This UNO bot has three game modes: Classic, Sanic and Wild.\n\n"
+modes_explanation = ("This UNO bot has four game modes: One, Classic, Sanic and Wild.\n\n"
                      " 🎻 The Classic mode uses the conventional UNO deck and there is no auto skip.\n"
                      " 🚀 The Sanic mode uses the conventional UNO deck and the bot automatically skips a player if he/she takes too long to play its turn\n"
                      " 🐉 The Wild mode uses a deck with more special cards, less number variety and no auto skip.\n"
