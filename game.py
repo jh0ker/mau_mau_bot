a464f366ebf7bea4f93b7d0315ba8fbcc841c3f9
Pesakitan22 <pesakitan22@about.me>
2018-04-18 09:47:32 +0700

    fix stats

--- game.py
+++ game.py
@@ -64,7 +64,7 @@ class Game(object):
         return players
 
     def start(self):
-        if self.mode == None or self.mode == "fast"
+        if self.mode == None or self.mode == 'fast'
             self.deck._fill_classic_()
         else:
             self.deck._fill_wild_()
