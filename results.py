c3e82827ba577ae50faf49a95b763b5ad7d98fff
David <30721971+dvlwj@users.noreply.github.com>
2018-07-16 09:41:58 +0700

    Update results.py
    change * to ** in player_list

--- results.py
+++ results.py
@@ -61,8 +61,8 @@ def add_other_cards(player, results, game):
 
 def player_list(game):
     """Generate list of player strings"""
-    return [_("*{name}* ({number} card)",
-              "*{name}* ({number} cards)",
+    return [_("**{name}** ({number} card)",
+              "**{name}** ({number} cards)",
               len(player.cards))
             .format(name=player.user.first_name, number=len(player.cards))
             for player in game.players]
