46bc6c4dbe93a75548037b53bd715bf1b30d4d5a
JuniorJPDJ <git@juniorjpdj.pl>
2020-12-23 16:59:12 +0100

    Changable db path

--- shared_vars.py
+++ shared_vars.py
@@ -20,12 +20,13 @@
 
 from config import TOKEN, WORKERS
 import logging
+import os
 from telegram.ext import Updater
 
 from game_manager import GameManager
 from database import db
 
-db.bind('sqlite', 'uno.sqlite3', create_db=True)
+db.bind('sqlite', os.getenv('UNO_DB', 'uno.sqlite3'), create_db=True)
 db.generate_mapping(create_tables=True)
 
 gm = GameManager()
