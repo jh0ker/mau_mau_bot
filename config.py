d8b73d6429a2d1b06bab670b38cbe90214d31231
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2024-02-18 19:13:29 +0530

    Config from environment
    checks for config.json if not available then uses vars from env

--- config.py
+++ config.py
@@ -18,18 +18,22 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
+import os
 import json
 
-with open("config.json","r") as f:
-    config = json.loads(f.read())
+try:
+    with open("config.json", "r") as f:
+        config = json.loads(f.read())
+except FileNotFoundError:
+    config = {}
 
-TOKEN=config.get("token")
-WORKERS=config.get("workers", 32)
-ADMIN_LIST = config.get("admin_list", None)
-OPEN_LOBBY = config.get("open_lobby", True)
-ENABLE_TRANSLATIONS = config.get("enable_translations", False)
-DEFAULT_GAMEMODE = config.get("default_gamemode", "fast")
-WAITING_TIME = config.get("waiting_time", 120)
-TIME_REMOVAL_AFTER_SKIP = config.get("time_removal_after_skip", 20)
-MIN_FAST_TURN_TIME = config.get("min_fast_turn_time", 15)
-MIN_PLAYERS = config.get("min_players", 2)
+TOKEN = os.getenv("TOKEN", config.get("token"))
+WORKERS = int(os.getenv("WORKERS", config.get("workers", 32)))
+ADMIN_LIST = os.getenv("ADMIN_LIST", config.get("admin_list", None))
+OPEN_LOBBY = os.getenv("OPEN_LOBBY", config.get("open_lobby", True)) == "True"
+ENABLE_TRANSLATIONS = os.getenv("ENABLE_TRANSLATIONS", config.get("enable_translations", False)) == "True"
+DEFAULT_GAMEMODE = os.getenv("DEFAULT_GAMEMODE", config.get("default_gamemode", "fast"))
+WAITING_TIME = int(os.getenv("WAITING_TIME", config.get("waiting_time", 120)))
+TIME_REMOVAL_AFTER_SKIP = int(os.getenv("TIME_REMOVAL_AFTER_SKIP", config.get("time_removal_after_skip", 20)))
+MIN_FAST_TURN_TIME = int(os.getenv("MIN_FAST_TURN_TIME", config.get("min_fast_turn_time", 15)))
+MIN_PLAYERS = int(os.getenv("MIN_PLAYERS", config.get("min_players", 2)))
